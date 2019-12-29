0e9e0d33113cae2f0f8838c3937d40f900127645
Unknown <SlaySayto@gmail.com>
2019-12-29 14:27:59 +0100

    Fix requirement
    exclude env from git 
    reorder the project structure

---
+++ GTK/cmake-build-debug/CMakeFiles/GTK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GTK.dir/src/crypt.c.o"
+  "CMakeFiles/GTK.dir/src/main.c.o"
+  "CMakeFiles/GTK.dir/src/trial.c.o"
+  "GTK.pdb"
+  "GTK"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GTK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GTK/cmake-build-debug/CMakeFiles/Kao.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kao"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Kao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
