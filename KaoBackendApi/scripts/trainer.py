bc872567e74786ec8128252e44e30a5751c25365
Unknown <SlaySayto@gmail.com>
2020-01-02 21:21:17 +0100

    update trainer again :(
    put the import in the header of the file

--- KaoBackendApi/scripts/trainer.py
+++ KaoBackendApi/scripts/trainer.py
@@ -1,3 +1,20 @@
+import os
+
+for path in ['/root/.virtualenvs/OpenCV-3.4.4-py3/bin', '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python35.zip',
+             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5',
+             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/plat-x86_64-linux-gnu',
+             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/lib-dynload', '/usr/lib/python3.5',
+             '/usr/lib/python3.5/plat-x86_64-linux-gnu',
+             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages',
+             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages/IPython/extensions',
+             '/root/.ipython']:
+    os.sys.path.append(path)
+import cv2
+import sys
+import numpy as np
+from PIL import Image
+import os
+
 # function to get the images and label data
 def getImagesAndLabels(path):
     imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
@@ -15,22 +32,6 @@ def getImagesAndLabels(path):
 
 
 def train_model():
-    import os
-
-    for path in ['/root/.virtualenvs/OpenCV-3.4.4-py3/bin', '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python35.zip',
-                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5',
-                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/plat-x86_64-linux-gnu',
-                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/lib-dynload', '/usr/lib/python3.5',
-                 '/usr/lib/python3.5/plat-x86_64-linux-gnu',
-                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages',
-                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages/IPython/extensions',
-                 '/root/.ipython']:
-        os.sys.path.append(path)
-    import cv2
-    import sys
-    import numpy as np
-    from PIL import Image
-    import os
 
     # Print the current working directory
     print(os.getcwd())
