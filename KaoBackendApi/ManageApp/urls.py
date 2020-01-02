0c1afbc08ab29541423d9741888a848d4280ecfe
Unknown <SlaySayto@gmail.com>
2020-01-02 21:19:51 +0100

    update trainer
    import openCv inside the trainer fn

--- KaoBackendApi/ManageApp/urls.py
+++ KaoBackendApi/ManageApp/urls.py
@@ -30,24 +30,6 @@ print("###################################")
 print("###  Importing OpenCV-3.4.4-py3 ###")
 print("###################################")
 print("")
-import os
 
-for path in ['/root/.virtualenvs/OpenCV-3.4.4-py3/bin', '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python35.zip',
-             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5',
-             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/plat-x86_64-linux-gnu',
-             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/lib-dynload', '/usr/lib/python3.5',
-             '/usr/lib/python3.5/plat-x86_64-linux-gnu',
-             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages',
-             '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages/IPython/extensions',
-             '/root/.ipython']:
-    os.sys.path.append(path)
-import cv2
-import sys
-import numpy as np
-from PIL import Image
-import os
-
-# Print the current working directory
-print(os.getcwd())
 
 train_model()
--- KaoBackendApi/scripts/trainer.py
+++ KaoBackendApi/scripts/trainer.py
@@ -15,7 +15,25 @@ def getImagesAndLabels(path):
 
 
 def train_model():
+    import os
 
+    for path in ['/root/.virtualenvs/OpenCV-3.4.4-py3/bin', '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python35.zip',
+                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5',
+                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/plat-x86_64-linux-gnu',
+                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/lib-dynload', '/usr/lib/python3.5',
+                 '/usr/lib/python3.5/plat-x86_64-linux-gnu',
+                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages',
+                 '/root/.virtualenvs/OpenCV-3.4.4-py3/lib/python3.5/site-packages/IPython/extensions',
+                 '/root/.ipython']:
+        os.sys.path.append(path)
+    import cv2
+    import sys
+    import numpy as np
+    from PIL import Image
+    import os
+
+    # Print the current working directory
+    print(os.getcwd())
     print("############################################")
     print("######  Training model is starting... ######")
     print("############################################")
